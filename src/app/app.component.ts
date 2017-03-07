d9b68784bd0068bffffac5fb98a061483daebe5d
Wanasak <u510610433@gmail.com>
2017-03-07 16:56:16 +0700

    Group sibling elements with <ng-container>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app/app.component.html'
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css']
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent { 
+  name = 'Angular'; 
+  hero = { name: 'Superman' };
+  heroes = [
+    { name: 'Batman', emotion: 'sad' },
+    { name: 'Superman', emotion: 'happy' },
+    { name: 'Spiderman', emotion: 'sad' }
+  ];
+}
